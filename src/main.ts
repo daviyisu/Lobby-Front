07fb1aaef550ed24f3193e4a52e8e3091a4a6f26
daviyisu <davidjrvt@hotmail.com>
2025-12-28 13:04:40 +0100

    Fix interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
-import { provideHttpClient, HttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  HttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { jwtInterceptor } from './services/jwt-interceptor.service';
 
 export function httpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
@@ -14,7 +19,7 @@ export function httpLoaderFactory(http: HttpClient) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
     importProvidersFrom(
       TranslateModule.forRoot({
         defaultLanguage: 'es',
