ed4bfac5289090c0715ff2dba367abd87d39d9bf
daviyisu <davidjrvt@hotmail.com>
2026-03-14 12:48:31 +0100

    Add first basic layout

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LobbyButtonComponent } from "../components/lobby-button/lobby-button.component";
+import { LoginService } from 'src/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-menu',
@@ -8,5 +10,11 @@ import { LobbyButtonComponent } from "../components/lobby-button/lobby-button.co
   styleUrl: './top-menu.component.scss',
 })
 export class TopMenuComponent {
+private loginService = inject(LoginService);
+private router = inject(Router);
 
+protected logout(): void {
+    this.loginService.logout();
+    this.router.navigateByUrl('login');
+  }
 }
